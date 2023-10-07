c65d11769dbeb4eb49cb3dafda3b8e19c0b0822e
Ahada <>
2023-10-07 18:55:07 +0530

    routing added bodyparser added

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const http = require('http');
-const express = require('express');
 const app = express();
 
-app.use((req,res,next)=>{ //Here next is function which allow middleware to travel to next middleware
-    console.log('aaaa')
-  res.send('<h1>Hiii</h1>')
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use("/add-product", (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit"> Add product</button> </form>');
+});
+
+app.use('/product', (req,res,next)=>{
+  console.log(req.body);
+  res.redirect('/');
 })
 
+app.use("/", (req, res, next) => {
+  res.send("<h1>Welcome page</h1>");
+});
 
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
